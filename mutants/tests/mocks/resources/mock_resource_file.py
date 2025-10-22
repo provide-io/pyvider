f2ce4ecfc5aed49e9f6a19114de2ff37cde56381
Tim <code@tim.life>
2025-10-22 09:52:27 -0700

    🔼⚙️ [skip ci] auto-commit

--- mutants/tests/mocks/resources/mock_resource_file.py
+++
@@ -1,7 +0,0 @@
-from pyvider.hub import register_resource
-
-
-@register_resource(category="test_category", name="mock_resource", description="A mock resource")
-class MockResource:
-    def __init__(self):
-        pass
--- mutants/tests/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Mock implementations for testing the core interfaces."""
-
-from .dummy_types import DummyType
-
-__all__ = [
-    "DummyType",
-]
