8e3dffdb416def6f370e9bc59ab2324ed2824c7b
Tim <code@tim.life>
2025-10-24 16:10:27 -0700

    🔼⚙️ [skip ci] auto-commit

--- src/pyvider/providers/__init__.py
+++ src/pyvider/providers/__init__.py
@@ -1,6 +1,6 @@
 # pyvider/src/pyvider/providers/__init__.py
 
-from pyvider.providers.base import BaseProvider
+from pyvider.providers.base import BaseProvider, ProviderMetadata
 from pyvider.providers.context import ProviderContext
 from pyvider.providers.decorators import register_provider
 from pyvider.providers.provider import PyviderProvider
@@ -8,6 +8,7 @@ from pyvider.providers.provider import PyviderProvider
 __all__ = [
     "BaseProvider",
     "ProviderContext",
+    "ProviderMetadata",
     "PyviderProvider",
     "register_provider",
 ]
--- src/pyvider/resources/__init__.py
+++ src/pyvider/resources/__init__.py
@@ -2,11 +2,13 @@
 # pyvider/resources/__init__.py
 #
 
+from pyvider.resources.base import BaseResource
 from pyvider.resources.context import ResourceContext
 from pyvider.resources.decorators import register_resource
 from pyvider.resources.private_state import PrivateState
 
 __all__ = [
+    "BaseResource",
     "PrivateState",
     "ResourceContext",
     "register_resource",
