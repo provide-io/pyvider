eea55a6524cf66b0c25be420d0f43f6004115233
Tim <code@tim.life>
2025-11-03 18:57:22 -0800

    🔼⚙️ [skip ci] auto-commit

--- tests/tfprotov6/handlers/test_read_resource.py
+++ tests/tfprotov6/handlers/test_read_resource.py
@@ -21,7 +21,7 @@ from pyvider.protocols.tfprotov6.handlers.read_resource import (
 import pyvider.protocols.tfprotov6.protobuf as pb
 from pyvider.resources.base import BaseResource, ResourceContext
 from pyvider.resources.private_state import PrivateState
-from pyvider.schema import Schema, a_num, a_str, s_resource
+from pyvider.schema import a_num, a_str, s_resource, PvsSchema as Schema
 
 
 @attrs.define
