d89d1442eb6a33970e563a5a6a8d03debdeaf551
Tim <code@tim.life>
2025-11-05 09:01:35 -0800

    🔼⚙️ [skip ci] auto-commit

--- src/pyvider/cli/main.py
+++ src/pyvider/cli/main.py
@@ -81,7 +81,7 @@ def cli(ctx: click.Context, **kwargs: Any) -> None:
                 click.echo(cli.get_help(ctx))
         else:
             # Not being run by Terraform - show interactive mode
-            click.echo(cli.get_help(ctx))
+            _show_interactive_mode(ctx)
 
 
 # This decorator is for our custom context object, which is correct for subcommands.
