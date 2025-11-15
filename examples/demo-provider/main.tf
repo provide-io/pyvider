5591223fea6c13c7f9857d7c3a6e93ad8e949cee
Tim <code@tim.life>
2025-11-14 19:23:38 -0800

    🔼⚙️ [skip ci] auto-commit

--- examples/demo-provider/main.tf
+++ examples/demo-provider/main.tf
@@ -1,17 +1,17 @@
 terraform {
   required_providers {
-    demo = {
-      source = "local/demo"
-      version = "1.0.0"
+    pyvider = {
+      source = "local/providers/pyvider"
     }
   }
 }
 
-provider "demo" {
+provider "pyvider" {
   api_key = "test-key"
 }
 
 resource "demo_resource" "test" {
+  provider    = pyvider
   name        = "test-resource"
   description = "Testing s_function with real Terraform"
   count       = 5
@@ -19,15 +19,15 @@ resource "demo_resource" "test" {
 
 # Test the upper function (using s_function!)
 output "test_upper" {
-  value = provider::demo::upper("hello from terraform")
+  value = provider::pyvider::upper("hello from terraform")
 }
 
 # Test the join_strings function (using s_function!)
 output "test_join" {
-  value = provider::demo::join_strings(["terraform", "pyvider", "s_function"], " + ")
+  value = provider::pyvider::join_strings(["terraform", "pyvider", "s_function"], " + ")
 }
 
 # Test the add function (using s_function!)
 output "test_add" {
-  value = provider::demo::add(42, 58)
+  value = provider::pyvider::add(42, 58)
 }
