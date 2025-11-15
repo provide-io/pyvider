cc85d9967f5b4ef43a5debbbfda8f4fa4dc5a97b
Tim <code@tim.life>
2025-11-14 19:35:33 -0800

    🔼⚙️ [skip ci] auto-commit

--- examples/demo-provider/main.tf
+++ examples/demo-provider/main.tf
@@ -1,16 +1,16 @@
 terraform {
   required_providers {
-    pyvider = {
-      source = "local/providers/pyvider"
+    demo = {
+      source = "local/providers/demo"
     }
   }
 }
 
-provider "pyvider" {}
+provider "demo" {}
 
 # Test format_tags function (using s_function!)
 output "test_format_tags" {
-  value = provider::pyvider::format_tags({
+  value = provider::demo::format_tags({
     Environment = "production"
     Project     = "s_function_test"
     Team        = "platform"
@@ -19,19 +19,19 @@ output "test_format_tags" {
 
 # Test calculate_cost function (using s_function!)
 output "test_calculate_cost" {
-  value = provider::pyvider::calculate_cost("t2.medium", 730)
+  value = provider::demo::calculate_cost("t2.medium", 730)
 }
 
 # Test validate_cidr function (using s_function!)
 output "test_validate_cidr_valid" {
-  value = provider::pyvider::validate_cidr("10.0.0.0/16")
+  value = provider::demo::validate_cidr("10.0.0.0/16")
 }
 
 output "test_validate_cidr_invalid" {
-  value = provider::pyvider::validate_cidr("not-a-cidr")
+  value = provider::demo::validate_cidr("not-a-cidr")
 }
 
 # Test generate_name function (using s_function!)
 output "test_generate_name" {
-  value = provider::pyvider::generate_name("web", "prod", "us-east-1", 42)
+  value = provider::demo::generate_name("web", "prod", "us-east-1", 42)
 }
