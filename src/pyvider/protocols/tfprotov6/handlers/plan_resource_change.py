ea17d19a7631b8bee3af541e6d1d83102e39cba1
Tim <code@tim.life>
2026-03-21 20:00:00 -0700

    harden(logging): guard debug calls with expensive argument construction

--- src/pyvider/protocols/tfprotov6/handlers/plan_resource_change.py
+++ src/pyvider/protocols/tfprotov6/handlers/plan_resource_change.py
@@ -168,8 +168,9 @@ def _handle_planned_state_dict(
     resource_schema: Any,
     response: pb.PlanResourceChange.Response,
 ) -> None:
-    logger.debug("_handle_planned_state_dict received", keys=list(planned_state_dict.keys()))
-    logger.debug("Planned state dict values", planned_state_dict=planned_state_dict)
+    if logger.is_debug_enabled():
+        logger.debug("_handle_planned_state_dict received", keys=list(planned_state_dict.keys()))
+        logger.debug("Planned state dict values", planned_state_dict=planned_state_dict)
 
     validator_type = resource_schema.block.to_cty_type()
     if not isinstance(validator_type, CtyObject):
@@ -197,7 +198,8 @@ def _handle_planned_state_dict(
     # Don't convert to None, as that creates null CtyValues which fail validation for required fields
     raw_values_for_validation = planned_state_dict.copy()
 
-    logger.debug("Raw values for validation", keys=list(raw_values_for_validation.keys()))
+    if logger.is_debug_enabled():
+        logger.debug("Raw values for validation", keys=list(raw_values_for_validation.keys()))
 
     # Validate the planned state - unknown values will be preserved by CTY
     planned_state_cty_final = validator_type.validate(raw_values_for_validation)
